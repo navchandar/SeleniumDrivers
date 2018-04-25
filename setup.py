3c66506eb32874abdf4080e290a12796da5584e2
Naveen <navchandar@users.noreply.github.com>
2018-04-25 15:51:53 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,24 +14,16 @@ setup(
     keywords=['testing', 'selenium', 'driver', 'webdrivers', 'update', 'automation'],  # arbitrary keywords
     classifiers=[
         'Development Status :: 3 - Alpha',
-
         'Intended Audience :: Developers',
         'Intended Audience :: Information Technology',
-        'Intended Audience :: System Administrators',
-
         'Topic :: Software Development :: Build Tools',
-
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-
         'Programming Language :: Python',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
-
         'Operating System :: Microsoft :: Windows :: Windows 7',
         'Topic :: Education :: Testing',
         'Topic :: Software Development :: Quality Assurance',
         'Topic :: Software Development :: Testing',
-        'Topic :: Utilities',
-
     ],
 )
