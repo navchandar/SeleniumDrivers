757a130221ffa1e839e1b6bdfc86f31e3410f79d
Naveen <navchandar@users.noreply.github.com>
2018-04-25 15:10:23 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,8 +7,19 @@ setup(
     description='A Python library or utility to download/update Selenium Driver files',
     author='Naveenchandar',
     author_email='naveen_sty@yahoo.in',
-    url='https://github.com/navchandar/SeleniumDrivers',  # use the URL to the github repo
+    # use the URL to the github repo
+    url='https://github.com/navchandar/SeleniumDrivers',
     download_url='https://github.com/navchandar/SeleniumDrivers/archive/0.1.tar.gz',
+    license='GPL3',
     keywords=['testing', 'selenium', 'driver', 'webdrivers', 'update', 'automation'],  # arbitrary keywords
-    classifiers=[],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+
+        'Intended Audience :: Developers, Testers',
+        'Topic :: Software Development :: Build Tools',
+
+        'License :: OSI Approved :: GPL3 License',
+
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6']
 )
