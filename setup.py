9835990e799adf09c263bed41f365afdcad526ba
Naveen <navchandar@users.noreply.github.com>
2018-04-25 15:21:57 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from distutils.core import setup
 setup(
     name='SeleniumDrivers',
     packages=['SeleniumDrivers'],  # this must be the same as the name above
-    version='0.1',
+    version='0.2',
     description='A Python library or utility to download/update Selenium Driver files',
     author='Naveenchandar',
     author_email='naveen_sty@yahoo.in',
     # use the URL to the github repo
     url='https://github.com/navchandar/SeleniumDrivers',
-    download_url='https://github.com/navchandar/SeleniumDrivers/archive/0.1.tar.gz',
+    download_url='https://github.com/navchandar/SeleniumDrivers/archive/0.2.tar.gz',
     license='GPL3',
     keywords=['testing', 'selenium', 'driver', 'webdrivers', 'update', 'automation'],  # arbitrary keywords
     classifiers=[
@@ -22,9 +22,4 @@ setup(
 
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6'],
-    include_package_data=True,
-    zip_safe=True,
-    install_requires=[
-        'beautifulsoup4',
-        'requests']
 )
